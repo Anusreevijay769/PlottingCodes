b1d810cc87e0952273f625464aff14dfe8b83de7
Anusree <anusree.vijay@cern.ch>
2024-09-25 18:29:47 +0200

    updates

--- ControlPlots/RunMacro_debug.C
+++ ControlPlots/RunMacro_debug.C
@@ -6,5 +6,5 @@ void RunMacro_debug()
     gROOT->ProcessLine(".L myControlPlots.C+");
     TString OutPutPath = "/eos/user/a/avijay/results_controlplot";
 
-    gROOT->ProcessLine("myControlPlots(\"HZZ2l2nu_Cuts_LowMET_VBF_ControlRegion_Muon.txt\", \"HZZ2l2nu_MuonSamples13TeV_DYinclusive_herwig_2024_07_01.txt\", controlplotvars_CHS_debug, \"OutDir_nobtag_VBF_MET100_Muon_drellyan_inclusive_herwig.root\", \"" + OutPutPath + "\"  \"OutDir_VBF_MET100_Muon_drellyan_inclusive_herwig\", 0) ");
+    gROOT->ProcessLine("myControlPlots(\"HZZ2l2nu_Cuts_LowMETControlRegion_MedBTag_NoVBF_ZEROJet.txt\", \"HZZ2l2nu_ElectronSamples13TeV_2024_06_27.txt\", controlplotvars_CHS_debug, \"zerojet_type1_ele.root\", \"" + OutPutPath + "\"  \"zerojet_type2_mtzz_ele\", 0) ");
 }
